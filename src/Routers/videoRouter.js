06dfe0754fefff4f7d9cc4813a647c704e2010aa
wihyanghoon <gidgns1995@gmail.com>
2023-04-04 21:20:34 +0900

    add: edit patch router

--- src/Routers/videoRouter.js
+++ src/Routers/videoRouter.js
@@ -1,64 +1,68 @@
-import express from "express"
-import Video from "../models/Video"
+import express from "express";
+import Video from "../models/Video";
 
-const router = express.Router()
-
+const router = express.Router();
 
 router.get("/", async (req, res) => {
-    console.log("start")
-    const result = Video.find({});
-    Video.find({}).then(()=> {
-        console.log("fdfd")
-        res.render("home", { pageTitle: "Home", videos: [] })
-    })
-    
-})
+  console.log("start");
+  const result = Video.find({});
+  Video.find({}).then(() => {
+    console.log("fdfd");
+    res.render("home", { pageTitle: "Home", videos: [] });
+  });
+});
 
-router.get("/:id(\\d+)", (req, res) => {
-    const { id } = req.params
-    const video = videos[id - 1]
-    res.render("watch", { pageTitle: `watching` })
-})
+router.get("/:id([0-9a-f]{24})", async (req, res) => {
+  const { id } = req.params;
+  const video = await Video.findById(id);
+  video
+    ? res.render("watch", { pageTitle: video.title, video })
+    : res.render("404", { pageTitle: "video not fonud" });
+  console.log(video);
+});
 
+router.get("/:id([0-9a-f]{24})/edit", async (req, res) => {
+  const { id } = req.params;
+  const video = await Video.findById(id);
+  if(!video) {
+    return res.render("404",{ pageTitle : "page is not found"})
+  }
+  res.render("edit", { pageTitle: "change video", video})
+});
 
-router.get("/:id(\\d+)/watch", (req, res) => {
-    res.render("home", { pageTitle: "Home" })
-})
-
-
-router.get("/:id(\\d+)/edit", (req, res) => {
-    const { id } = req.params
-    const video = videos[id - 1]
-    res.render("edit", { pageTitle: `Edit` })
-})
-
-router.post("/:id(\\d+)/edit", (req, res) => {
-    const { id } = req.params
-    const { title } = req.body
-    videos[id - 1].title = title
-    console.log(title)
-    res.redirect(`/videos/${id}`)
-})
+router.get("/:id([0-9a-f]{24})/edit", async (req, res) => {
+    const { id } = req.params;
+    const video = await Video.findById(id);
+    if(!video) {
+      return res.render("404",{ pageTitle : "page is not found"})
+    }
+    res.render("edit", { pageTitle: "change video", video})
+  });
 
 router.get("/upload", (req, res) => {
-    res.render("upload", { pageTitle: "upload" })
-})
+  res.render("upload", { pageTitle: "upload" });
+});
 
 // 비디오 추가 로직
 router.post("/upload", async (req, res) => {
-    const { title, description, hashtags} = req.body
-    const video = new Video({
-        title,
-        description,
-        createAt: Date.now(),
-        hashTags: hashtags.split(",").map((item) => `#${item}`),
-        meta: {
-            views: 0,
-            rating: 0,
-        }
-    })
-    await video.save()
-    return res.redirect("/")
-})
+  const { title, description, hashtags } = req.body;
+  try {
+    const dbVideo = await Video.create({
+      title,
+      description,
+      hashTags: hashtags
+      .split(",")
+      .map((item) => (item.startsWith("#") ? item : `#${word}`)),
+    });
+    console.log(dbVideo);
+    return res.redirect("/");
+  } catch (error) {
+    console.log(error);
+    return res.render("upload", {
+      pageTitle: "Upload Video",
+      errorMessage: error._message,
+    });
+  }
+});
 
-export default router
+export default router;
--- src/models/Video.js
+++ src/models/Video.js
@@ -1,17 +1,16 @@
 import mongoose from "mongoose";
 
 const videoSchema = new mongoose.Schema({
-    title: String,
-    description: String,
-    createAt: Date,
-    hashTags: [{ type: String }],
-    meta: {
-        views: Number,
-        rating: Number,
-    }
-})
+  title: { type: String, required: true, trim: true, maxLength: 80,},
+  description: { type: String, required: true, trim: true, minLength: 20,},
+  createAt: { type: Date, required: true, default: Date.now },
+  hashTags: [{ type: String, trim: true }],
+  meta: {
+    views: { type: Number, required: true, default: 0 },
+    rating: { type: Number, required: true, default: 0 },
+  },
+});
 
+const Video = mongoose.model("Video", videoSchema);
 
-const Video = mongoose.model("Video", videoSchema)
-
-export default Video
+export default Video;
